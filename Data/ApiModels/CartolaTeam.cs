984748747be5665137f280e561287f3492777751
Edson Candido <Edson.Candido@howden.com>
2024-09-26 17:11:33 -0300

    cartola team

---
+++ Data/ApiModels/CartolaTeam.cs
@@ -0,0 +1,16 @@
+﻿namespace LigaHowden.Data.ApiModels
+{
+    public class CartolaTeam
+    {
+        public string url_escudo_svg { get; set; }
+        public string nome_cartola { get; set; }
+        public string slug { get; set; }
+        public string url_escudo_png { get; set; }
+        public string nome { get; set; }
+        public string foto_perfil { get; set; }
+        public string descricao { get; set; }
+        public string facebook_id { get; set; }
+        public int time_id { get; set; }
+
+    }
+}
